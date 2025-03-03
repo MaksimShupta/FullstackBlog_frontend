b88665a9c498226e51c39577a61151f6749e22ae
Razvan Andrei <chiper_raizy@yahoo.com>
2025-03-03 09:13:38 +0100

    work of right side

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import { MdOutlineMail } from "react-icons/md"; //  import MdOutlineMail from react-icons/md
-import { GiCoffeeCup } from "react-icons/gi"; //import GiCoffeeCup from react-icons/gi
-import { GrMagic } from "react-icons/gr"; //import GrMagic from react-icons/gr
+import { FaInstagram } from "react-icons/fa"; // import FaInstagram from react-icons/fa
+import { SlSocialFacebook } from "react-icons/sl"; // import SlSocialFacebook from react-icons/sl
+import { Link } from "react-router"; //importing Link from react-router
 const Footer = () => {
   return (
     <footer className=" p-10 bg-primary w-full">
@@ -14,11 +15,19 @@ const Footer = () => {
         {/* end of left side */}
 
         {/* right side */}
-        <div className="flex flex-row items-start sm:items-end sm:justify-end text-white mt-10 sm:mt-0">
+        <div className="flex flex-row text-lg  mt-5 items-start sm:items-end sm:justify-end text-white sm:mt-0">
           <p className="mr-4 ">Get in touch:</p>
-          <div className="flex flex-row gap-3  ">
-            <MdOutlineMail />
-            <GrMagic />
+          <div className="flex flex-row gap-2 mb-1  ">
+            <Link to="https://mail.google.com/mail/">
+              <MdOutlineMail />
+            </Link>
+            {/* <Link to="https://www.instagram.com/"> */}
+              <FaInstagram />
+            {/* </Link> */}
+
+            <Link to="https://www.facebook.com/">
+              <SlSocialFacebook />
+            </Link>
           </div>
         </div>
         {/* end of right side */}
