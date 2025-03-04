2e303ed972cdbc1641ef308d4fd4010c65875492
Razvan Andrei <chiper_raizy@yahoo.com>
2025-03-04 07:20:43 +0100

    edditing the footer icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { SlSocialFacebook } from "react-icons/sl"; // import SlSocialFacebook fr
 import { Link } from "react-router"; //importing Link from react-router
 const Footer = () => {
   return (
-    <footer className=" p-10 bg-primary w-full">
+    <footer className="flex  p-10 bg-primary w-full">
       <div className="container flex flex-col sm:flex-row mx-auto justify-between">
         {/* left side */}
         <div className="">
@@ -15,17 +15,17 @@ const Footer = () => {
         {/* end of left side */}
 
         {/* right side */}
-        <div className="flex flex-row text-2xl  mt-5 items-start sm:items-end sm:justify-end text-white sm:mt-0">
-          <p className="mr-4 sm:mb-2">Get in touch:</p>
-          <div className="flex flex-row  gap-2   ">
+        <div className="flex flex-row text-2xl items-start sm:items-end sm:justify-end text-white sm:mt-0 ">
+          <p className="mr-3  text-lg  sm:mb-2 sm:mt-[1.2rem]">Get in touch:</p>
+          <div className="flex flex-row  gap-2 mb-[0.4rem] ">
             <Link to="https://mail.google.com/mail/">
-              <MdOutlineMail size={50}/>
+              <MdOutlineMail size={30}  />
             </Link>
-            <Link to="https://www.instagram.com/">
-              <FaInstagram size={50} />
+            <Link to="https://www.instagram.com/" >
+              <FaInstagram size={28} />
             </Link>
             <Link to="https://www.facebook.com/" >
-              <SlSocialFacebook size={50} />
+              <SlSocialFacebook size={25} />
             </Link>
           </div>
         </div>
