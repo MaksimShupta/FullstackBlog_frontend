3f3312a446079e2e91fa1fa2b42a29c5f0aa0427
Razvan Andrei <chiper_raizy@yahoo.com>
2025-03-04 10:52:10 +0100

    fix text git in app.jsx

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
         primary: "#DDEB9D", // Main color (dark green) for buttons and key UI elements
         secondary: "#A0C878", // Secondary color (light blue) for tags and subtle UI elements
         accent: "#27667B", // Accent color (yellow) for highlights and ratings
-        bgSoft: "#",
+        bgSoft: "#143D60",
       },
       fontFamily: {
         sans: ["Roboto", "sans-serif"], // Set Roboto as the default sans-serif font
