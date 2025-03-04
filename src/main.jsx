4ff7d23a06be810134e1183b145ae74ca5b6bf3f
Anne Martinez <martinezannemerlyn@gmail.com>
2025-03-04 15:23:36 +0100

    updated: fix issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,11 @@
 // main.jsx
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter as Router } from 'react-router-dom';
-import { CategoryProvider } from './context/CategoryContext';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Router>
-      <CategoryProvider>
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
         <App />
-      </CategoryProvider>
-    </Router>
-  </React.StrictMode>,
-)
+    </StrictMode>
+);
